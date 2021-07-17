4765eef78c6d8c5e14009b4e04cbedb6962b8df5
kenux <kenux.yun@gmail.com>
2021-07-17 15:12:38 +0900

    로그아웃

--- src/main/java/org/kenux/security/config/SecurityConfig.java
+++ src/main/java/org/kenux/security/config/SecurityConfig.java
@@ -9,10 +9,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Slf4j
@@ -36,18 +39,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .successHandler(new AuthenticationSuccessHandler() {
                     @Override
                     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        log.info("authentication: " + authentication.getName());
+                        log.debug("authentication: " + authentication.getName());
                         response.sendRedirect("/");
                     }
                 })
                 .failureHandler(new AuthenticationFailureHandler() {
                     @Override
                     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                        log.info("exception: " + exception.getMessage());
+                        log.debug("exception: " + exception.getMessage());
                         response.sendRedirect("/login");
                     }
                 })
-                .permitAll() // 위 경로들은 모두 접근 가능
-        ;
+                .permitAll(); // 위 경로들은 모두 접근 가능
+
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        log.debug("로그아웃 핸들러 => 세션 삭제");
+                        HttpSession session = request.getSession();
+                        session.invalidate(); // 세션 삭제
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        log.debug("로그아웃 성공 => send redirect('/login')");
+                        response.sendRedirect("/login"); // 로그아웃 성공 후 로그인 페이지로 이동
+                    }
+                })
+                .deleteCookies("remember-me"); // 쿠키 삭제
     }
 }
