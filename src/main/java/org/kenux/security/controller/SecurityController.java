a664a4185b021c314f7bf0267e500be1b710deb7
kenux <kenux.yun@gmail.com>
2021-07-23 00:01:06 +0900

    SecurityContext, SecurityContextHolder

--- src/main/java/org/kenux/security/controller/SecurityController.java
+++ src/main/java/org/kenux/security/controller/SecurityController.java
@@ -1,14 +1,33 @@
 package org.kenux.security.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
-        return "home";
+    public ResponseEntity<List<Authentication>> index(HttpSession httpSession) {
+
+        List<Authentication> authentications = new ArrayList<>();
+        authentications.add(SecurityContextHolder.getContext().getAuthentication());
+        SecurityContext context =
+                (SecurityContext) httpSession.getAttribute(
+                        HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        authentications.add(context.getAuthentication());
+
+        return ResponseEntity.ok(authentications);
     }
 
     @GetMapping("/loginPage")
