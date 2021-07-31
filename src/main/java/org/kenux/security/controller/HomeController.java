7ec495c9a1670b9bd37180d12186b359bb694e7b
kenux <kenux.yun@gmail.com>
2021-08-01 01:43:40 +0900

    Add HomeController

--- src/main/java/org/kenux/security/controller/HomeController.java
+++ src/main/java/org/kenux/security/controller/HomeController.java
@@ -1,4 +1,18 @@
 package org.kenux.security.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping(value = "/")
+    public String home() throws Exception {
+        return "home";
+    }
+
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
 }
